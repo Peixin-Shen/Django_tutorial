43fe3188a6a84ca418e3522be99dafd4c48af127
Peixin <b54066067@gs.ncku.edu.tw>
2020-08-11 23:20:34 +0800

    listing

--- backend/blog/views.py
+++ backend/blog/views.py
@@ -1,6 +1,11 @@
 #from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Blog
+
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the blog index.")
+    blogs = Blog.objects.all()
+    return render(request, 'index.html', {'blogs':blogs})
+    # return HttpResponse("Hello, world. You're at the blog index.")
 # Create your views here.
